545505ac63e6a76bce4aec9f32b4c56dcb41bb16
mahmoudsharkawey <ma7moudelsharkawey@gmail.com>
2024-07-31 15:02:50 +0300

    change styling for the card

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -5,59 +5,59 @@ import Image from "next/image";
 export const revalidate = 30;
 
 const BlogCard = async () => {
-  const data: blogCard[] = await getData();
-  return (
-    <div>
-      {data.map((post, idx) => {
-        return (
-          <div className="sm:flex p-8 justify-between " key={idx}>
-            <div className="flex items-center space-x-4">
-              <div className="flex-1">
-                <Link href={`/blog/${post.currentSlug}`}>
-                  <h1 className="text-3xl font-bold hover:underline hover:cursor-pointer">
-                    {post.title}
-                  </h1>
-                </Link>
-                <p className="mt-2 text-sm text-gray-400">
-                  {post._createdAt}
-                  <span aria-hidden="true"> </span>
-                  <ClockIcon className="inline-block w-4 h-4 text-gray-400" />
-                </p>
-                <p className="mt-4 text-lg text-gray-500 ">{post.smallDescription}</p>
-              </div>
-            </div>
+    const data: blogCard[] = await getData();
+    return (
+        <div>
+            {data.map((post, idx) => {
+                return (
+                    <div className="sm:flex p-8 m-10 justify-between ease-in duration-300 border-2 hover:border-sky-500 rounded-2xl" key={idx}>
+                        <div className="flex items-center space-x-4">
+                            <div className="flex-1">
+                                <Link href={`/blog/${post.currentSlug}`}>
+                                    <h1 className="text-3xl font-bold hover:underline hover:cursor-pointer">
+                                        {post.title}
+                                    </h1>
+                                </Link>
+                                <p className="mt-2 text-sm text-gray-400">
+                                    {post._createdAt}
+                                    <span aria-hidden="true"> </span>
+                                    <ClockIcon className="inline-block w-4 h-4 text-gray-400" />
+                                </p>
+                                <p className="mt-4 text-lg text-gray-500 ">{post.smallDescription}</p>
+                            </div>
+                        </div>
 
-            <Image
-              className="rounded-xl w-[100%] sm:w-48"
-              src={urlFor(post.titleImage).url()}
-              alt={`${post.title}-cover `}
-              width={200}
-              height={200}
-            />
-          </div>
-        );
-      })}
-    </div>
-  );
+                        <Image
+                            className="rounded-xl w-[100%] sm:w-52 ml-8"
+                            src={urlFor(post.titleImage).url()}
+                            alt={`${post.title}-cover `}
+                            width={200}
+                            height={200}
+                        />
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 export default BlogCard;
 
 function ClockIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <polyline points="12 6 12 12 16 14" />
-    </svg>
-  );
+    return (
+        <svg
+            {...props}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <circle cx="12" cy="12" r="10" />
+            <polyline points="12 6 12 12 16 14" />
+        </svg>
+    );
 }
